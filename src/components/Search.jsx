7a586028b7f5a2261d4bab0428a533518057330e
kyoungholee <dlrudgh1303@gmail.com>
2023-06-06 00:20:53 +0900

    채팅하고 싶은 유저를 찾고, 대화 내용을 저장 할 수 있게 db 생성

---
+++ src/components/Search.jsx
@@ -0,0 +1,111 @@
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    //firebase에서 제공하는 query코드
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      //firebase에서 제공하는 query 사용한 코드
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    //그룹이 존재하는지 확인하는 영역
+    const combinedId =
+      //currentUser는 현재 로그인된 유저
+      // user는 input창에 내가 입력한 유저
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      //chats db를 가져옴
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        //chats라는 db를 생성
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //userChats db를 업데이트해준다.
+        // 로그인한 본인과 대화한 내용이 저장됌
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        //상대방과 대화한 내용이 저장됌
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+
+    setUser(null);
+    setUsername("");
+  };
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="상대방을 찾아보자"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          onClick={() => {
+            err && alert("해당 사용자가 없습니다.");
+          }}
+        />
+      </div>
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
